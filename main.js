73d52db9d60f7f5f3a51055086d70c6ae026a8db
ryderwishart <ryderwishart@gmail.com>
2023-08-10 22:10:54 -0700

    Fix duplicate XML doc population; use classes and tag names

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const checkboxState = {
     tagName: {},
     className: {},
-    label: {},
 };
 const tagColors = {};
-const labelColors = {};
 
 // Load all XML files from the xml/ subdirectory at build time
 async function loadXMLFiles() {
@@ -14,48 +12,48 @@ async function loadXMLFiles() {
     return xmlFiles;
 }
 
-document.addEventListener("DOMContentLoaded", async function () {
-    // Create file selector
-    const fileSelector = document.getElementById("file-selector");
-    const select = document.createElement("select");
-    select.addEventListener("change", async () => {
-        // Clear current XML content and stylesheet
-        document.getElementById("xml-display").innerHTML = "";
-        document.getElementById("checkbox-container").innerHTML = "";
-        const oldStylesheet = document.querySelector("link[rel=stylesheet]");
-        if (oldStylesheet) {
-            oldStylesheet.remove();
-        }
+// document.addEventListener("DOMContentLoaded", async function () {
+//     // Create file selector
+//     const fileSelector = document.getElementById("file-selector");
+//     const select = document.createElement("select");
+//     select.addEventListener("change", async () => {
+//         // Clear current XML content and stylesheet
+//         document.getElementById("xml-display").innerHTML = "";
+//         document.getElementById("checkbox-container").innerHTML = "";
+//         // const oldStylesheet = document.querySelector("link[rel=stylesheet]");
+//         // if (oldStylesheet) {
+//         //     oldStylesheet.remove();
+//         // }
 
-        // Fetch and parse selected file
-        const selectedFile = select.value;
-        const { stylesheet, xmlDoc } = await parseXMLFile(selectedFile); // Destructure xmlDoc from the result
+//         // Fetch and parse selected file
+//         const selectedFile = select.value;
+//         const { stylesheets, xmlDoc } = await parseXMLFile(selectedFile);
 
-        loadStylesheet(stylesheet);
+//         for (const stylesheet of stylesheets) {
+//             await Promise.all(stylesheets.map(loadStylesheet));
+//         }
 
-        // Repopulate XML content and stylesheet
-        const { uniqueTags, uniqueLabels } = populateXMLDisplay(xmlDoc);
-        addDynamicStyles(uniqueTags, uniqueLabels);
-        populateCheckboxes(xmlDoc);
-    });
+//         // Repopulate XML content and stylesheet
+//         const { uniqueTags, uniqueLabels } = populateXMLDisplay(xmlDoc);
+//         // addDynamicStyles(uniqueTags, uniqueLabels);
+//         populateCheckboxes(xmlDoc);
+//     });
 
+//     fileSelector.appendChild(select);
 
+//     // Load all XML files and populate file selector
+//     const xmlFiles = await loadXMLFiles();
+//     xmlFiles.forEach((file) => {
+//         const option = document.createElement("option");
+//         option.value = file;
+//         option.text = file;
+//         select.appendChild(option);
+//     });
 
-    fileSelector.appendChild(select);
-
-    // Load all XML files and populate file selector
-    const xmlFiles = await loadXMLFiles();
-    xmlFiles.forEach((file) => {
-        const option = document.createElement("option");
-        option.value = file;
-        option.text = file;
-        select.appendChild(option);
-    });
-
-    // Select the first file by default
-    select.value = xmlFiles[0];
-    select.dispatchEvent(new Event("change"));
-});
+//     // Select the first file by default
+//     select.value = xmlFiles[0];
+//     select.dispatchEvent(new Event("change"));
+// });
 
 
 
@@ -68,59 +66,59 @@ async function parseXMLFile(file) {
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(fileData, "text/xml");
 
-        let stylesheet;
+        let stylesheets = [];
         for (let i = 0; i < xmlDoc.childNodes.length; i++) {
             const node = xmlDoc.childNodes[i];
             if (node.nodeType === 7 && node.target === 'xml-stylesheet') { // nodeType 7 is for processing instructions
                 const match = node.data.match(/href="([^"]*)"/);
-                stylesheet = match ? match[1] : undefined;
-                break;
+                if (match) {
+                    stylesheets.push(match[1]);
+                }
             }
         }
 
-        console.log('parseXMLFile', { stylesheet, xmlDoc });
-        return { stylesheet, xmlDoc };
+        console.log('parseXMLFile', { stylesheets, xmlDoc });
+        return { stylesheets, xmlDoc };
     } catch (error) {
         console.error(`Error parsing file ${file}:`, error);
     }
 }
 
 
-
-
 function loadStylesheet(stylesheet) {
-    const linkElement = document.createElement("link");
-    linkElement.rel = "stylesheet";
-    linkElement.href = stylesheet;
-    document.head.appendChild(linkElement);
-
-    console.log('loadStylesheet', { stylesheet });
+    return new Promise((resolve) => {
+        const linkElement = document.createElement("link");
+        linkElement.rel = "stylesheet";
+        linkElement.href = stylesheet;
+        linkElement.onload = () => {
+            console.log('Stylesheet loaded', { stylesheet });
+            resolve();
+        };
+        document.head.appendChild(linkElement);
+    });
 }
 
 
 // Generate checkboxes based on the content of the selected XML file
 function populateCheckboxes(xmlDoc) {
     const checkboxesContainer = document.getElementById("checkbox-container");
-    const xmlNodes = Array.from(xmlDoc.querySelectorAll("*")); // Get all elements in the XML except header
+    const xmlNodes = Array.from(xmlDoc.querySelectorAll("*"));
 
     const uniqueTagNames = new Set();
     const uniqueClassNames = new Set();
-    const uniqueLabels = new Set();
 
     xmlNodes.forEach((node) => {
-        // Ignore the <header> node and all descendants of <header>
         if (node.closest("header")) return;
         uniqueTagNames.add(node.tagName);
         node.className && uniqueClassNames.add(node.className);
-        node.getAttribute("label") && uniqueLabels.add(node.getAttribute("label"));
+
+        // Add .{tagName} to node
+        node.classList.add(node.tagName);
     });
 
-    // Initialize checkboxState properties
     checkboxState.tagName = {};
     checkboxState.className = {};
-    checkboxState.label = {};
 
-    // Function to create a checkbox for a given value and type
     const createCheckbox = (value, type) => {
         const label = document.createElement("label");
         const checkbox = document.createElement("input");
@@ -134,28 +132,14 @@ function populateCheckboxes(xmlDoc) {
         checkbox.checked = true;
         checkbox.dataset[type] = value;
 
-        // add color decoration to the checkbox label
-        if (type === 'tagName') {
-            label.style.color = tagColors[value];
-        } else if (type === 'label') {
-            label.style.color = labelColors[value];
-        }
-
-        checkbox.addEventListener("change", () => {
-            checkboxState[type][value] = checkbox.checked;
-            updateDisplay();
-        });
         label.appendChild(checkbox);
         label.appendChild(document.createTextNode(`${type}: ${value}`));
         checkboxesContainer.appendChild(label);
     };
 
-    // Add a heading and checkboxes for each type
-    ["Tag Names", "Class Names", "Labels"].forEach((heading, i) => {
-        const uniqueSet = [uniqueTagNames, uniqueClassNames, uniqueLabels][i];
-        const type = ["tagName", "className", "label"][i];
-
-        // Only create heading and checkboxes if there are unique values
+    ["Tag Names", "Class Names"].forEach((heading, i) => {
+        const uniqueSet = [uniqueTagNames, uniqueClassNames][i];
+        const type = ["tagName", "className"][i];
         if (uniqueSet.size > 0) {
             const h3 = document.createElement("h3");
             h3.textContent = heading;
@@ -163,36 +147,32 @@ function populateCheckboxes(xmlDoc) {
             uniqueSet.forEach(value => createCheckbox(value, type));
         }
     });
-
-    console.log('populateCheckboxes', { xmlNodes });
-}
-
-function toggleDisplay(type, value) {
-    const nodes = Array.from(document.querySelectorAll(`#xml-display .${type}-${value}`));
-    nodes.forEach((node) => {
-        const hideClass = type === 'tagName' ? 'hideTag' : 'hideLabel';
-        if (node.classList.contains(hideClass)) {
-            node.classList.remove(hideClass);
-        } else {
-            node.classList.add(hideClass);
-        }
-    });
 }
 
 function updateDisplay() {
-    const nodes = Array.from(document.querySelectorAll("#xml-display *"));
-    nodes.forEach((node) => {
-        const types = ["tagName", "label"];
-        node.classList.remove("hideTag");
-        node.classList.remove("hideLabel");
-        for (const type of types) {
-            const value = type === 'tagName' ? node[type] : node.getAttribute(type) || "no-label";
-            const hideClass = type === 'tagName' ? 'hideTag' : 'hideLabel';
-            if (checkboxState[type][value] === false) {
-                node.classList.add(hideClass);
+    for (const tagName in checkboxState.tagName) {
+        const hide = checkboxState.tagName[tagName] === false;
+        const elements = document.querySelectorAll(`#xml-display .${tagName}`);
+        elements.forEach((element) => {
+            if (hide) {
+                element.classList.add('hideTag');
+            } else {
+                element.classList.remove('hideTag');
             }
-        }
-    });
+        });
+    }
+
+    for (const className in checkboxState.className) {
+        const hide = checkboxState.className[className] === false;
+        const elements = document.querySelectorAll(`#xml-display .${className}`);
+        elements.forEach((element) => {
+            if (hide) {
+                element.classList.add('hideClass');
+            } else {
+                element.classList.remove('hideClass');
+            }
+        });
+    }
 }
 
 // Populate tooltips or status bar for functions/values on hovering an element
@@ -206,6 +186,7 @@ function populateTooltips(node) {
 }
 
 function populateXMLDisplay(xmlDoc) {
+    debugger;
     console.log({ xmlDoc })
     const xmlDisplayContainer = document.getElementById("xml-display");
     const uniqueNodes = new Set();
@@ -214,6 +195,7 @@ function populateXMLDisplay(xmlDoc) {
 
     // Clone the entire XML document into the "xml-display" container
     const clonedNode = xmlDoc.documentElement.cloneNode(true);
+    xmlDisplayContainer.innerHTML = "";
     xmlDisplayContainer.appendChild(clonedNode);
 
     // Scan for unique nodes
@@ -224,14 +206,12 @@ function populateXMLDisplay(xmlDoc) {
 
         if (!uniqueNodes.has(identifier)) { // only process unique nodes
             uniqueNodes.add(identifier);
-            node.classList.add(`tagName-${node.tagName}`);
-            node.classList.add(`label-${labelAttr}`);
+            // node.classList.add(`tagName-${node.tagName}`);
+            // node.classList.add(`label-${labelAttr}`);
             uniqueTags.add(node.tagName);
             uniqueLabels.add(labelAttr);
         }
     });
-
-    console.log('populateXMLDisplay', { xmlNodes });
     return { uniqueTags, uniqueLabels };
 }
 
@@ -240,17 +220,16 @@ function addDynamicStyles(uniqueTags, uniqueLabels) {
     let styles = '';
 
     // Add styles for unique tags
-    uniqueTags.forEach((tag, i) => {
+    uniqueTags.forEach((tag) => {
         const color = randomColor();
         tagColors[tag] = color; // store the color for each tag
-        styles += `.tag-${tag} { border: 2px solid ${color}; border-radius: 5px; padding: 5px; }\n`;
+        styles += `.${tag} { border: 2px solid ${color}; border-radius: 5px; padding: 5px; }\n`;
     });
 
-    // Add styles for unique labels
-    uniqueLabels.forEach((label, i) => {
+    // Add styles for unique class names
+    uniqueClassNames.forEach((className) => {
         const color = randomColor();
-        labelColors[label] = color; // store the color for each label
-        styles += `.label-${label} { outline: 2px solid ${color}; }\n`;
+        styles += `.${className} { outline: 2px solid ${color}; }\n`;
     });
 
     styleElement.textContent = styles;
@@ -274,16 +253,12 @@ async function main() {
     const xmlFiles = await loadXMLFiles();
     console.log('main', { xmlFiles });
     const selectedFile = xmlFiles[0]; // Select the first file by default
-    const parsedFile = await parseXMLFile(selectedFile);
-    const stylesheet = parsedFile.stylesheet;
-    loadStylesheet(stylesheet);
-    const xmlDoc = parsedFile.xmlDoc;
-    const { uniqueTags, uniqueLabels } = populateXMLDisplay(xmlDoc); // Pass xmlDoc instead of xmlNodes
+    const { stylesheets, xmlDoc } = await parseXMLFile(selectedFile);
+    await Promise.all(stylesheets.map(loadStylesheet));
+    const { uniqueTags, uniqueLabels } = populateXMLDisplay(xmlDoc);
+    populateCheckboxes(xmlDoc);
     addDynamicStyles(uniqueTags, uniqueLabels);
-    populateCheckboxes(xmlDoc); // Pass xmlDoc instead of xmlNodes
 }
-
-
 
 
 // Run the main function when the page loads
